013e3e3fea435fd73e4607083c4b17b7867762c2
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-07-02 18:35:27 +0300

    ex. 4 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,90 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+char** num_number_matrix(const char* str, int& num_numbers_row) {
+    int rows = 50;
+    int cols = 2;
+    char** matrix = new char* [rows]; // Initialize letter matrix
+    for (int i = 0; i < rows; ++i) {
+        matrix[i] = new char[cols];
+        matrix[i][0] = '\0'; // Initialize with null character
+        matrix[i][1] = 0; // Initialize count to 0
+    }
+
+    int str_len = strlen(str);
+
+    for (int i = 0; i < str_len; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            bool is_in_matrix = false;
+            for (int j = 0; j < num_numbers_row; j++) {
+                if (str[i] == matrix[j][0]) {
+                    matrix[j][1]++; // Increment count
+                    is_in_matrix = true;
+                    break;
+                }
+            }
+            if (!is_in_matrix) {
+                matrix[num_numbers_row][0] = str[i];
+                matrix[num_numbers_row][1] = 1; // Initialize count to 1
+                num_numbers_row++;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+int count_number_in_text(const char* str) {
+    int num_of_numbers = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9') {
+            num_of_numbers++;
+        }
+    }
+    return num_of_numbers;
+}
+
+template<typename T>
+void printMatrix(T** arr, int row, int col = 2) {
+    cout <<"Number --- count of number" << endl;
+
+    for (size_t i = 0; i < row; i++) {
+        for (size_t j = 0; j < col; j++) {
+            if (j == 1) {
+                cout << static_cast<int>(arr[i][j]) << " "; // Print count as integer
+            }
+            else {
+                cout << arr[i][j] << "    ---    ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void delete_matrix(char** matrix, int rows) {
+    for (int i = 0; i < rows; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+int main() {
+    char str[] = "Lorem ipsum 2 dolor do 3 sit amet.1 do ,hello consectetur adipiscing 3 elit hello. Nam accumsan, hello, 2 ex do dodo varius cursus 3 egestas, lacus hello tellus convallis nisi, ut porttitor!!!";
+    cout << str << endl << endl;
+
+    int row_matrix = 0;
+
+    char** matrix = num_number_matrix(str, row_matrix);
+
+    printMatrix(matrix, row_matrix);
+
+    cout << endl << "Count of numbers in text: " << count_number_in_text(str) << endl;
+
+    delete_matrix(matrix, row_matrix);
+
+    
+
+    return 0;
 }
